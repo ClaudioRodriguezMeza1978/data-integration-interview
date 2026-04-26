e29929b46760074fdcabacfdc2dac55b95a95027
ClaudioRodriguezFitWork <claudio.rodriguez@fitwork.cl>
2026-04-26 13:33:00 -0400

    limpie todo en cero

--- vmetrix/database.sql
+++ vmetrix/database.sql
@@ -10,3 +10,17 @@ CREATE TABLE BANXICO_SERIES(
   CIFRA VARCHAR,
   UNIDAD VARCHAR
 );
+
+CREATE TABLE IF NOT EXISTS tmp_raw_banxico (
+    idSerie VARCHAR,
+    fecha VARCHAR,
+    dato VARCHAR,
+    sw_reglaNeg int,
+    obs_reglaNeg varchar
+);
+
+CREATE TABLE IF NOT EXISTS tbl_hist_banxico (
+    idSerie VARCHAR,
+    fecha DATE,
+    dato decimal(18,6)
+);
